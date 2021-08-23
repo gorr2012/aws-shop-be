c983a7533f52f9d5823261db1bf7d460e2afaa9b
gorr2012 <sergey_gornost@mail.ru>
2021-08-23 12:38:05 +0300

    fix: add some config

--- product-service/tests/functions/getProductsList/getProductsList.test.ts
+++ product-service/tests/functions/getProductsList/getProductsList.test.ts
@@ -1,73 +1,7 @@
 import { getProductsList } from '../../../src/functions/getProductsList/getProductsList';
 
-const products = [
-  {
-    "count": 4,
-    "description": "Short Product Description1",
-    "id": "7567ec4b-b10c-48c5-9345-fc73c48a80aa",
-    "price": 2.4,
-    "title": "ProductOne"
-  },
-  {
-    "count": 6,
-    "description": "Short Product Description3",
-    "id": "7567ec4b-b10c-48c5-9345-fc73c48a80a0",
-    "price": 10,
-    "title": "ProductNew"
-  },
-  {
-    "count": 7,
-    "description": "Short Product Description2",
-    "id": "7567ec4b-b10c-48c5-9345-fc73c48a80a2",
-    "price": 23,
-    "title": "ProductTop"
-  },
-  {
-    "count": 12,
-    "description": "Short Product Description7",
-    "id": "7567ec4b-b10c-48c5-9345-fc73c48a80a1",
-    "price": 15,
-    "title": "ProductTitle"
-  },
-  {
-    "count": 7,
-    "description": "Short Product Description2",
-    "id": "7567ec4b-b10c-48c5-9345-fc73c48a80a3",
-    "price": 23,
-    "title": "Product"
-  },
-  {
-    "count": 8,
-    "description": "Short Product Description4",
-    "id": "7567ec4b-b10c-48c5-9345-fc73348a80a1",
-    "price": 15,
-    "title": "ProductTest"
-  },
-  {
-    "count": 2,
-    "description": "Short Product Descriptio1",
-    "id": "7567ec4b-b10c-48c5-9445-fc73c48a80a2",
-    "price": 23,
-    "title": "Product2"
-  },
-  {
-    "count": 3,
-    "description": "Short Product Description7",
-    "id": "7567ec4b-b10c-45c5-9345-fc73c48a80a1",
-    "price": 15,
-    "title": "ProductName"
-  },
-  {
-    "count": 3,
-    "description": "Short Product Description7",
-    "id": "7567ec4b-b10c-45c5-9345-fc73c48a80a2",
-    "price": 15,
-    "title": "The best product"
-  }
-]
-
 describe('Should return right Error', () => {
   test('adds 1 + 2 to equal 3', () => {
-    expect(getProductsList()).toBe(3);
+    expect(getProductsList({}, 200)).toBe(3);
   });
 });
